95f498053a203523b823a20831b86e8f61ea6089
abelardo <abelardo.diaz@pucp.pe>
2019-12-30 01:31:38 -0500

    Auto stash before merge of "master" and "origin/master"

--- eica/eica/urls.py
+++ eica/eica/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.eica.views import index
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$',index),
 ]
