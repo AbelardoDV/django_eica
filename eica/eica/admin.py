7112d945a1a55290c6c58a32bf491b3242e3f9a8
abelardo <abelardo.diaz@pucp.pe>
2020-01-07 23:34:50 -0500

    admin,py

--- eica/eica/admin.py
+++ eica/eica/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-# from .models import Personas
+from django.apps import apps
 
 # class PersonasAdmin(admin.ModelAdmin):
 #     fieldsets   =  [(None, {'fields':['nombre','edad','dni']}),('Date information', {'fields':['fecha_creado']})]
 #     list_display = ('nombre','fecha_creado')
-# admin.site.register(Personas,PersonasAdmin)
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
