d9dbc1198e7a347a959003c45d50ae600b4612eb
abelardo <abelardo.diaz@pucp.pe>
2020-01-30 15:13:50 -0500

    ya se procesa correctamente el fitlrado de datepicker

--- eica/eica/urls.py
+++ eica/eica/urls.py
@@ -66,7 +66,7 @@ urlpatterns = [
     path('compras_productos/', compras_productos_view, name='compras_productos'),
     path('compras_historial/', compras_historial_view, name='compras_historial'),
     path('compras/actualizar_valido/', actualizar_boletaCompra_valido_ajax, name='actualizar_valido'),
-    path('compras/actualizar_valido/', importe_compra_fecha_rango_ajax, name='actualizar_valido'),
+    path('compras/importe_compra_fecha_rango/', importe_compra_fecha_rango_ajax, name='importe_compra_fecha_rango'),
     # -----------------Fin seccion de Compra-----------------
 
     # -----------------Inicio seccion agregar plato-----------------
--- eica/eica/views.py
+++ eica/eica/views.py
@@ -14,9 +14,11 @@ from .models import ProductoPlato
 from .models import PlatoPadre
 from .models import PlatoHijoVenta
 from .models import BoletaVentaRestaurante
-
+###########################################################
 from django.utils.timezone import get_current_timezone
 import datetime
+##########################################################
+from django.db.models import Count
 
 # -------------------------Inicio Dashboard-------------------------
 
@@ -240,8 +242,17 @@ def actualizar_boletaCompra_valido_ajax(request):
 #Aquí se procesa AJAX para Importe BoletaCompra
 @login_required(login_url='/accounts/login')
 def importe_compra_fecha_rango_ajax(request):
- 
-    return HttpResponse(status=200)
+    response_data={}
+    if request.method == 'POST':
+        print(request.POST.get('startDate'))
+        print(request.POST.get('endDate'))
+        startDate= datetime.datetime.strptime(request.POST.get('startDate'), "%Y-%m-%d")
+        endDate= datetime.datetime.strptime(request.POST.get('endDate'), "%Y-%m-%d")
+        print(startDate)
+        print(endDate)
+        boletas=BoletaCompra.objects.filter(valido=True,fecha_compra__range=[startDate,endDate]).annotate(cantidad=Count('id'))
+        response_data['cantidad_boletas']=boletas[0].cantidad
+    return JsonResponse(response_data)
 # ---------------------------------Fin Seccion Productos---------------------------------
 
 
