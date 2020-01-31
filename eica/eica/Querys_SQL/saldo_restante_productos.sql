2c83b8664fccff5f427f51cebdb2453c15980bc1
abelardo <abelardo.diaz@pucp.pe>
2020-01-31 13:15:07 -0500

    mejorando query de saldo de productos

--- eica/eica/Querys_SQL/saldo_restante_productos.sql
+++ eica/eica/Querys_SQL/saldo_restante_productos.sql
@@ -3,8 +3,8 @@ SELECT
 	productos_comprados.nombre,
 	productos_comprados.unidad,
 	productos_comprados.cantidad__sum as cantidad_comprada,
-	productos_vendidos.cantidad as cantidad_vendida,
-	productos_comprados.cantidad__sum - productos_vendidos.cantidad as saldo_restante,
+	coalesce(productos_vendidos.cantidad,0) as cantidad_vendida,
+	productos_comprados.cantidad__sum - coalesce(productos_vendidos.cantidad,0) as saldo_restante,
 	ROUND((100*productos_vendidos.cantidad/productos_comprados.cantidad__sum) ::numeric,2) as porcentaje_consumo
 FROM productos_comprados 
 LEFT JOIN productos_vendidos 
--- eica/eica/views.py
+++ eica/eica/views.py
@@ -62,13 +62,13 @@ def dashboard_reporte_economico_view(request):
     nombre_vista = 'Dashboard | Reporte económico'
     ruta_vista = ['Dashboard', 'Reporte económico']
     saldo_restante_productos=ProductoPadre.objects.raw("""SELECT
-                                productos_comprados.id,
-                                productos_comprados.nombre,
-                                productos_comprados.unidad,
-                                productos_comprados.cantidad__sum as cantidad_comprada,
-                                productos_vendidos.cantidad as cantidad_vendida,
-                                productos_comprados.cantidad__sum - productos_vendidos.cantidad as saldo_restante,
-                                ROUND((100*productos_vendidos.cantidad/productos_comprados.cantidad__sum) ::numeric,2) as porcentaje_consumo
+                            productos_comprados.id,
+                            productos_comprados.nombre,
+                            productos_comprados.unidad,
+                            productos_comprados.cantidad__sum as cantidad_comprada,
+                            coalesce(productos_vendidos.cantidad,0) as cantidad_vendida,
+                            productos_comprados.cantidad__sum - coalesce(productos_vendidos.cantidad,0) as saldo_restante,
+                            ROUND((100*productos_vendidos.cantidad/productos_comprados.cantidad__sum) ::numeric,2) as porcentaje_consumo
                             FROM productos_comprados 
                             LEFT JOIN productos_vendidos 
                                 ON productos_vendidos.id = productos_comprados.id""")
